bb28ca598cceeb0ef2bcbd4c5bd20a041e9f22f8
takuya <108344446+takuya4520@users.noreply.github.com>
2024-02-10 15:25:03 +0900

    ユーザー登録/ログイン/ログアウト実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  skip_before_action :require_login, only: %i[create new]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path
+    else#
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation, :name, :salt)
+   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: %i[new create]
 end
