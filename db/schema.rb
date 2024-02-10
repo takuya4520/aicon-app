c383b915d196b57ebd5de433558afff16c28f67f
takuya <108344446+takuya4520@users.noreply.github.com>
2024-02-10 09:03:54 +0900

    バグ修正

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,4 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_09_222235) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
