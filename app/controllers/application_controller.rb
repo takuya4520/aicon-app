aaf7b792d47bcdd0cf06744c4859e308b0d7ca7f
takuya <108344446+takuya4520@users.noreply.github.com>
2024-02-10 14:43:18 +0900

    login機能の叩き台

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
+  private
+
+  def not_authenticated
+    redirect_to login_path, danger: "ログインしてください"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
- root "tops#index"
+  root "tops#index"
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
