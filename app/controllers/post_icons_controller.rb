540e4a1ba1c2a6a3925f3cc295cf06447a15ff26
takuya <108344446+takuya4520@users.noreply.github.com>
2024-02-11 14:07:14 +0900

    投稿一覧機能追加

--- app/controllers/post_icons_controller.rb
+++ app/controllers/post_icons_controller.rb
@@ -1,5 +1,7 @@
 class PostIconsController < ApplicationController
+  before_action :set_posticon, only: %i[destroy]
   def index
+    @posticons = PostIcon.all.includes(:user).order(created_at: :desc)
   end
 
   def new
@@ -11,13 +13,14 @@ class PostIconsController < ApplicationController
     @posticon = current_user.post_icons.build(posticon_params)
     if @posticon.save
       flash[:success] = "Micropost created!"
-      redirect_to root_url
+      redirect_to post_icons_url
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def show
+    @posticon= PostIcon.find(params[:id])
   end
 
   def edit
@@ -27,9 +30,14 @@ class PostIconsController < ApplicationController
   end
 
   def destroy
+    @posticon.destroy!
+    redirect_to post_icons_path
   end
 
   private
+    def set_posticon
+      @posticon = current_user.post_icons.find(params[:id])
+    end
 
     def posticon_params
       params.require(:post_icon).permit(:title)
