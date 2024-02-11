eb91fed6475a8940016e2e2c1edb5d61578866b7
takuya <108344446+takuya4520@users.noreply.github.com>
2024-02-11 13:34:28 +0900

    投稿機能作成

--- app/controllers/post_icons_controller.rb
+++ app/controllers/post_icons_controller.rb
@@ -1,2 +1,38 @@
 class PostIconsController < ApplicationController
+  def index
+  end
+
+  def new
+    @posticon = PostIcon.new
+  end
+
+  def create
+    logger.debug params.inspect 
+    @posticon = current_user.post_icons.build(posticon_params)
+    if @posticon.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def posticon_params
+      params.require(:post_icon).permit(:title)
+    end
+
 end
