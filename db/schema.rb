953f428aba46270f9a15393a14e53b0fba36e728
takuya <108344446+takuya4520@users.noreply.github.com>
2024-02-14 20:11:41 +0900

    add:creatediconsにstatusカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_14_110553) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_14_111049) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_14_110553) do
     t.string "taste", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0, null: false
     t.index ["user_id", "created_at"], name: "index_created_icons_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_created_icons_on_user_id"
   end
