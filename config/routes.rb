6b722951d477d65cec9c3d41cbb2004f7ba501b5
takuya <108344446+takuya4520@users.noreply.github.com>
2024-02-15 21:31:50 +0900

    add: マイページ叩き台

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create show edit update]
   resources :post_icons
   resources :created_icons, only: %i[new create show destroy]
   resources :icons, only: %i[index]
