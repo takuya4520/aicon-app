973f856f6070e2f5bce3fe16033ad09abc8c907a
takuya <108344446+takuya4520@users.noreply.github.com>
2024-02-18 09:29:18 +0900

    edit: 投稿一覧のデザイン

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,15 +2,3 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 
-document.addEventListener("turbo:load", function () { // Turbolinks 5以前では 'turbolinks:load'
-  document.getElementById("menuButton").addEventListener("click", function () {
-    var menu = document.getElementById("menu");
-    if (menu.classList.contains("hidden")) {
-      menu.classList.remove("hidden");
-      menu.classList.add("block");
-    } else {
-      menu.classList.add("hidden");
-      menu.classList.remove("block");
-    }
-  });
-});
