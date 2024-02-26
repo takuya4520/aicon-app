b0636f11bd281c2cefca36b424f6583b9b89e0ac
takuya <108344446+takuya4520@users.noreply.github.com>
2024-02-26 21:25:17 +0900

    fix: mailのSubjectの修正

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,6 +5,6 @@ class UserMailer < ApplicationMailer
     @user = User.find(user.id)
     @url  = edit_password_reset_url(@user.reset_password_token)
     mail(to: user.email,
-         subject: t('defaults.password_reset'))
+         subject: 'パスワードリセット')
   end
 end
