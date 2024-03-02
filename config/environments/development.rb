1f4015e675f4a513e988f3c59dac4a5315d63578
takuya <108344446+takuya4520@users.noreply.github.com>
2024-03-02 16:57:17 +0900

    fix: 本番環境用設定の変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,21 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.hosts << "iconya-app.onrender.com"
+  config.action_mailer.delivery_method = :letter_opener_web # 送信方法を指定
   config.action_mailer.default_url_options = {host: 'localhost:3000'}
-
-  config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:"smtp.gmail.com",
-    domain: 'gmail.com',
-    port: 587,
-    user_name: Rails.application.credentials.gmail_username,
-    password: Rails.application.credentials.gmail_password,
-    authentication: 'plain',
-    enable_starttls_auto: true 
-  }
-
+  config.action_mailer.perform_deliveries = true # メールを実際に送信するかどうかを指定
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
