5ed57bfeadfbb763939b3c2f9b27795d1282215c
takuya <108344446+takuya4520@users.noreply.github.com>
2024-03-02 21:24:26 +0900

    edit: rakeに変更

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 rm -f /app/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
-bundle exec rails assets:precompile RAILS_ENV=production
-bundle exec rails assets:clean RAILS_ENV=production
-bundle exec rails db:migrate RAILS_ENV=production
+bundle exec rake assets:precompile RAILS_ENV=production
+bundle exec rake assets:clean RAILS_ENV=production
+bundle exec rake db:migrate RAILS_ENV=production
 
 exec "$@"
