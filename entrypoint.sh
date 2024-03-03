9c7fbe8dfb11c108770be687556d346bb244d626
takuya <108344446+takuya4520@users.noreply.github.com>
2024-03-03 10:31:47 +0900

    add: assets:clobberの追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 rm -f /app/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
+RAILS_ENV=production bundle exec rake assets:clobber
 RAILS_ENV=production bundle exec rake assets:precompile
 RAILS_ENV=production bundle exec rake assets:clean
 RAILS_ENV=production bundle exec rake db:migrate
