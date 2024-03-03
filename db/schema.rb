aab97c961c0055df11ca5c4f1413b49023a12617
takuya <108344446+takuya4520@users.noreply.github.com>
2024-03-03 22:35:01 +0900

    add: CreatedIconLikesモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_24_031021) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_03_092401) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_24_031021) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "created_icon_likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_icon_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "post_icon_id"], name: "index_created_icon_likes_on_user_id_and_post_icon_id", unique: true
   end
 
   create_table "created_icons", force: :cascade do |t|
