0081379535b6897d22ff24b2a4c49c41a0cde967
takuya <108344446+takuya4520@users.noreply.github.com>
2024-03-06 22:29:27 +0900

    add: いいね機能のajax化

--- app/controllers/created_icon_likes_controller.rb
+++ app/controllers/created_icon_likes_controller.rb
@@ -1,21 +1,13 @@
 class CreatedIconLikesController < ApplicationController
-  before_action :set_created_icon
+
 
   def create
-    @created_icon_like = CreatedIconLike.new(user_id: current_user.id,  created_icon_id: @created_icon.id)
-    @created_icon_like.save
-    redirect_to icons_path
+    @created_icon = CreatedIcon.find(params[:icon_id])
+    current_user.created_icon_like(@created_icon)
   end
 
   def destroy
-    @created_icon_like = current_user.created_icon_likes.find(params[:id])
-    @created_icon_like.destroy
-    redirect_to icons_path, status: :see_other
-  end
-
-  private
-
-  def set_created_icon
-    @created_icon = CreatedIcon.find_by(id: params[:icon_id])
+    @created_icon= current_user.created_icon_likes.find(params[:id]).created_icon
+    current_user.created_icon_unlike(@created_icon)
   end
 end
