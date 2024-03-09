bae9bf83e89bc713c63ee943440325adfd81f014
takuya <108344446+takuya4520@users.noreply.github.com>
2024-03-09 12:36:43 +0900

    add: ログインログアウトのフラッシュメッセージの追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,18 +1,21 @@
 class UserSessionsController < ApplicationController
-  skip_before_action :require_login, only: %i[create new destroy]
+  skip_before_action :require_login, only: %i[create new]
   def new; end
 
   def create
     @user = login(params[:email], params[:password])
     if @user
+      flash[:success] = "ログインしました"
       redirect_back_or_to root_path
     else
-      render :new
+      flash.now[:error] = 'ログインに失敗しました'
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     logout
+    flash[:success] = "ログアウトしました"
     redirect_to root_path
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+   def flash_background_color(type)
+    case type.to_sym
+    when :notice then "bg-info"
+    when :alert  then "bg-warning"
+    when :error  then "bg-error"
+    else "bg-success"
+    end
+  end
 end
