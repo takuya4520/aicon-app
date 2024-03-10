0067b16615ba6b59d3ad1e37dff709a9a120704d
takuya <108344446+takuya4520@users.noreply.github.com>
2024-03-10 21:43:28 +0900

    add: ゲストログイン機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+  post '/guest_login', to: 'user_sessions#guest_login'
 
   resources :users, only: %i[new create show edit update]
   resources :post_icons do
