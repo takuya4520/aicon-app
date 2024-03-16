e3c3a3d48f28e79f3ade37db07d84b2d7fa9a548
takuya <108344446+takuya4520@users.noreply.github.com>
2024-03-16 12:46:13 +0900

    add: topページのモーダル追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     if @user.save
       login(user_params[:email], user_params[:password])
       flash[:success] = "ログインしました"
-      redirect_to root_path
+      redirect_to new_created_icon_path
     else
       flash.now[:error] = '新規登録に失敗しました'
       render :new
