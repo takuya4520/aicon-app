b1b2435c0552bf96d9d20a2d9f3e72fb5b3db95b
takuya <108344446+takuya4520@users.noreply.github.com>
2024-03-17 15:44:22 +0900

    add: Xシェア機能

--- app/controllers/created_icons_controller.rb
+++ app/controllers/created_icons_controller.rb
@@ -1,4 +1,5 @@
 class CreatedIconsController < ApplicationController
+  skip_before_action :require_login, only: %i[show]
   before_action :set_createdicon, only: %i[edit update destroy]
 
   def new
