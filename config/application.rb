5cfda008520e6738d5729b7e816c20220eee27aa
takuya <108344446+takuya4520@users.noreply.github.com>
2024-03-20 19:20:24 +0900

    add: userモデルの国際化

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :ja
   end
 end
