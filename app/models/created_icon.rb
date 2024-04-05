8a52f4b3973c86432106ff57057b913a6c7582e4
takuya <108344446+takuya4520@users.noreply.github.com>
2024-04-05 22:05:57 +0900

    edit: 画像のリサイズを設定

--- app/models/created_icon.rb
+++ app/models/created_icon.rb
@@ -7,9 +7,13 @@ class CreatedIcon < ApplicationRecord
   validates :title, presence: true
   validates :icon, presence: true
 
-  validates :icon,   content_type: { in: %w[image/jpeg image/gif image/png],
+  validates :icon,   content_type: { in: %w[image/jpeg image/gif image/png image/webp],
                                       message: "must be a valid icon format" },
                       size:         { less_than: 5.megabytes,
                                       message:   "should be less than 5MB" }
   enum status: { published: 0, unpublished: 1 }
+
+  def thumbnail
+    return self.icon.variant(resize: '300x300').processed
+  end
 end
