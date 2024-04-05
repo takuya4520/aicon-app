220f9bcc0189fad31867509a8e21228bcc243f1f
takuya <108344446+takuya4520@users.noreply.github.com>
2024-04-05 22:03:46 +0900

    edit: 生成された画像をwebpで保存

--- app/models/post_icon.rb
+++ app/models/post_icon.rb
@@ -7,7 +7,7 @@ class PostIcon < ApplicationRecord
   validates :icon, presence: true
   has_many :post_icon_likes, dependent: :destroy
 
-  validates :icon,   content_type: { in: %w[image/jpeg image/gif image/png],
+  validates :icon,   content_type: { in: %w[image/jpeg image/gif image/png image/webp],
                                       message: "must be a valid icon format" },
                       size:         { less_than: 5.megabytes,
                                       message:   "should be less than 5MB" }
