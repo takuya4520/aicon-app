1cfd455fc3225d6f195358f85985b1d5de2c3444
takuya <108344446+takuya4520@users.noreply.github.com>
2024-04-11 20:20:41 +0900

    add: 繰り返し文字のバリデーション

--- app/models/created_icon.rb
+++ app/models/created_icon.rb
@@ -3,17 +3,24 @@ class CreatedIcon < ApplicationRecord
   has_one_attached :icon do |attachable|
     attachable.variant :display, resize_to_limit: [200, 200]
   end
+  
   has_many :created_icon_likes, dependent: :destroy
-  validates :title, presence: true
-  validates :icon, presence: true
-
+  
+  #繰り返し禁止バリデーション
+  NGWORD_REGEX = /(.)\1{4,}/.freeze
+ 
+  with_options presence: true do 
+    with_options format: { without: NGWORD_REGEX, message: 'は5文字以上の繰り返しは禁止です' } do
+      validates :title
+    end
+    validates :icon
+  end
   validates :icon,   content_type: { in: %w[image/jpeg image/gif image/png image/webp],
                                       message: "must be a valid icon format" },
                       size:         { less_than: 5.megabytes,
                                       message:   "should be less than 5MB" }
   enum status: { published: 0, unpublished: 1 }
 
-  def thumbnail
-    return self.icon.variant(resize: '300x300').processed
-  end
+  NGWORD_REGEX = /(.)\1{4,}/.freeze
+
 end
