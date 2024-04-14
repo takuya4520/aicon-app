9a21777dc3dd9c159ace90414105784f44e12ba7
takuya <108344446+takuya4520@users.noreply.github.com>
2024-04-14 21:36:37 +0900

    fix: blacklistの関数の修正

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,9 +4,12 @@ class ApplicationRecord < ActiveRecord::Base
   def title_cannot_contain_blacklist_words
     # 環境変数からブラックリストの単語を取得し、配列に変換
     blacklist = ENV['BLACKLIST_WORDS'].to_s.split(',')
+
     # titleが空でなく、titleの中にblacklistの用語が含まれているかをチェック
-    if title.present? && blacklist.any?{ |word| title.include?(word) }
-      errors.add(:contain_blacklist_words, ": 不適切な言葉は使用できません")
+    title_words = title.downcase.split(/\s+/)
+    if title.present? && blacklist.any? { |word| title_words.include?(word.downcase.strip) }
+      errors.add(:title, "不適切な言葉は使用できません")
     end
   end
+
 end
