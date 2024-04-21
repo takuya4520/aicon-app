67d57c7bec1afc455534d0e162b6aeb08d6ca8c6
takuya <108344446+takuya4520@users.noreply.github.com>
2024-04-21 10:33:12 +0900

    fix: db:seedの削除

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,5 @@ RAILS_ENV=production bundle exec rake assets:clobber
 RAILS_ENV=production bundle exec rake assets:precompile
 RAILS_ENV=production bundle exec rake assets:clean
 RAILS_ENV=production bundle exec rake db:migrate
-RAILS_ENV=production bundle exec rake db:seed
 
 exec "$@"
