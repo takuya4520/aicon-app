609909cdd4ed32640a6a5e35b0ff3445b276fbd3
takuya <108344446+takuya4520@users.noreply.github.com>
2024-04-22 20:05:04 +0900

    add: コメントの追加

--- app/controllers/icons_controller.rb
+++ app/controllers/icons_controller.rb
@@ -2,25 +2,28 @@ class IconsController < ApplicationController
   skip_before_action :require_login, only: %i[index]
 
   def index
+    #検索キーワード＋テイストの絞り込み
     if params[:search].present? && params[:taste].present?
-      #検索キーワード＋テイストの絞り込み
+      #N+1問題対策
       created_icons = CreatedIcon.where("title LIKE ? AND taste = ?", "%#{params[:search]}%", params[:taste]).where(status: :published).includes(:user)
       icons = created_icons.sort_by(&:created_at).reverse
     
-
+    #テイストの絞り込み
     elsif params[:taste].present?
-      #テイストの絞り込み
+      #N+1問題対策
       created_icons = CreatedIcon.where(taste: params[:taste]).where(status: :published).includes(:user)
       icons = created_icons.sort_by(&:created_at).reverse
 
+    #検索キーワードの絞り込み
     elsif params[:search].present?
-      #検索キーワードの絞り込み
+      #N+1問題対策
       post_icons = PostIcon.where("title LIKE ?", "%#{params[:search]}%").where(status: :published).includes(:user)
       created_icons = CreatedIcon.where("title LIKE ?", "%#{params[:search]}%").where(status: :published).includes(:user)
       icons = (post_icons + created_icons).sort_by(&:created_at).reverse
 
+    #検索キーワードとテイストの絞り込みが存在しない場合
     else
-      # 検索キーワードが存在しない場合、すべてのアイコンを取得
+      #N+1問題対策
       post_icons = PostIcon.where(status: :published).includes(:user)
       created_icons = CreatedIcon.where(status: :published).includes(:user)
       icons = (post_icons + created_icons).sort_by(&:created_at).reverse
--- app/controllers/like_icons_controller.rb
+++ app/controllers/like_icons_controller.rb
@@ -1,7 +1,10 @@
 class LikeIconsController < ApplicationController
   def index
+    #N+1問題対策
     post_icon_like_icons = current_user.post_icon_like_icons.includes(:user)
+    #N+1問題対策
     created_icon_like_icons = current_user.created_icon_like_icons.includes(:user)
+    #post_icon+created_icon
     like_icons = (post_icon_like_icons + created_icon_like_icons).sort_by(&:created_at).reverse
     @like_icons = Kaminari.paginate_array(like_icons).page(params[:page]).per(10)
   end
