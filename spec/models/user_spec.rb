5541567b25ab593c2b2df2ded092c469b2b396c8
takuya <108344446+takuya4520@users.noreply.github.com>
2024-06-19 12:34:15 +0900

    FactoryBotのセットアップ

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションチェック' do
+    it '設定したすべてのバリデーションが機能しているか' do
+      user = build(:user)
+      expect(user).to be_valid
+      expect(user.errors).to be_empty
+    end
+  end
 end
